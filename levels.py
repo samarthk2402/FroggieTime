4282cc439c80c47cdb6eadda09a8ac4f2d4b8d3f
sammydoodles <97102757+sammydoodles@users.noreply.github.com>
2022-01-13 18:21:00 +0000

    Add files via upload

---
+++ levels.py
@@ -0,0 +1,55 @@
+level_layouts = []
+
+level1 = [
+    "                C                          C            C              ",
+    "   C                      C                                    C       ",
+    "         C              A     SS      C         C                      ",
+    "                   C        TTTTT                                      ",
+    "                   AAAA     XXX        C              AA               ",
+    "             C     TTTT      XX             S         TT               ",
+    "                     XX                     TT                   C     ",
+    "   C     AAA        TXX            C    SS                         E   ",
+    "         TTT                         AATTT         C                   ",
+    "    A  TTXXX   AA T       A   T      TTXXX                       TTTT  ",
+    "               TTTX    SS T   XTT        X       SS      AT        XX  ",
+    "   TT    AA           TTTTX   XXX        X      TTT     ATX         X  ",
+    " FP   SSTTT       TT TXXXXX   XXXTT             XXX     TXX            ",
+    "TTTTTTTTXXXTT   TTXX XXXXXX   XXXXX                     X              "
+]
+
+level_layouts.append(level1)
+
+level2 = [
+    "              C                                                        ",
+    "                                   C                     C             ",
+    "                        C                 AA       A                   ",
+    "    C         S                           TT      TT            E      ",
+    "             TTTTA                       TXX            S              ",
+    "    FP   C   XXXX A          C                         AT      TTT     ",
+    "   TTT         XX  A                           C      ATX              ",
+    "               XX S  S            AA   AS   SS       ATXX              ",
+    "        AA     XXTTTTTT     AA    TT   TT   TTAA     TXXX              ",
+    "        TT     XXXXX        TT    XX   XX   XXTT                       ",
+    "                            XX    XX   XX     XX   AA                  ",
+    "             C             TXX    XX   XX          TT                  ",
+    "                           XXX    XX   XX                              ",
+    "                           XXX    XX   XX                              "
+]
+level_layouts.append(level2)
+
+level3 = [
+    "                  C            AS                    CAA               ",
+    "               AA              TT           C         TT               ",
+    "               TT              C                              E        ",
+    "       C                AS                     A                       ",
+    "                        TT                     T             TTT       ",
+    "            A                        AAA                      XX       ",
+    "    C       T      AA       A       STTTS                              ",
+    "                   TT       T      ATXXXTA          A      AS          ",
+    "       FP                          TXXXXXT          T      TT          ",
+    "     TTTT            SA         SSTXXXXXXXTSS                          ",
+    "                     TT         TTXXXXXXXXXTT                          ",
+    "            A                           C                              ",
+    "            T                                                          ",
+]
+level_layouts.append(level3)
